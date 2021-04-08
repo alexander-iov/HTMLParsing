856e4b6e7f1e010f4076638ec77a8b410612bb52
alexander-iov <sasha.iovsa@gmail.com>
2021-04-08 11:29:45 +0300

    Init commit

---
+++ src/main/java/Article.java
@@ -0,0 +1,39 @@
+public class Article {
+
+    private String name;
+    private double cost;
+    private String url;
+    private String about;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+}
